e5b3ce340752abf633f1b2ec31580f0501b83c36
https://github.com/somykm <sk43@hood.edu>
2024-08-22 01:39:39 -0400

    creating func can display api data into home page

--- front/js/script.js
+++ front/js/script.js
@@ -7,32 +7,32 @@ fetch('http://localhost:3000/api/products')
     insertItems(items);
   });
 
-const itemsHolder = document.getElementById('items');
-function insertItems(items) {
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-    console.log(item);
-
-    const productElement = document.createElement('a');
-    productElement.href = `./product.html?id=${item._id}`;//mls4
-
-    const articleElement = document.createElement('article');
-    productElement.appendChild(articleElement);
-
-    const imageElement = document.createElement('img');
-    imageElement.setAttribute('src', item.imageUrl);
-    imageElement.setAttribute('alt', item.altTxt);
-
-    articleElement.appendChild(imageElement);
-
-    //this line display item's name and description
-    articleElement.innerHTML += `
-      <h3 class="productName">${item.name}</h3>
-      <p class="productDescription">${item.description}</p>
-    `;
-
-
-    itemsHolder.appendChild(productElement);
+  const itemsHolder = document.getElementById('items');
+  function insertItems(items) {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      console.log(item);
+  
+      const productElement = document.createElement('a');
+      productElement.href = `./product.html?id=${item._id}`;//mls4
+  
+      const articleElement = document.createElement('article');
+      productElement.appendChild(articleElement);
+  
+      const imageElement = document.createElement('img');
+      imageElement.setAttribute('src', item.imageUrl);
+      imageElement.setAttribute('alt', item.altTxt);
+  
+      articleElement.appendChild(imageElement);
+  
+      //this line display item's name and description
+      articleElement.innerHTML += `
+        <h3 class="productName">${item.name}</h3>
+        <p class="productDescription">${item.description}</p>
+      `;
+  
+  
+      itemsHolder.appendChild(productElement);
+    }
+  
   }
-
-}
