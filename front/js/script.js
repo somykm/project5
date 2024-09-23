e002439b355d4d635d7524420af820f6de474d4b
https://github.com/somykm <sk43@hood.edu>
2024-09-22 21:25:49 -0400

    the last touch

--- front/js/script.js
+++ front/js/script.js
@@ -7,34 +7,34 @@ fetch('http://localhost:3000/api/products')
     insertItems(items);
   });
 
-  const itemsHolder = document.getElementById('items');
+const itemsHolder = document.getElementById('items');
 
-  /**
-   * display selected item's image, name, description on product page
-   * 
-   * @param {object} items 
-   */
-  function insertItems(items) {
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-      console.log(item);
-  
-      const productElement = document.createElement('a');
-      productElement.href = `./product.html?id=${item._id}`;
-  
-      const articleElement = document.createElement('article');
-      productElement.appendChild(articleElement);
-  
-      const imageElement = document.createElement('img');
-      imageElement.setAttribute('src', item.imageUrl);
-      imageElement.setAttribute('alt', item.altTxt);
-  
-      articleElement.appendChild(imageElement);
+/**
+ * display selected item's image, name, description on product page
+ * 
+ * @param {object} items 
+ */
+function insertItems(items) {
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    console.log(item);
 
-      articleElement.innerHTML += `
+    const productElement = document.createElement('a');
+    productElement.href = `./product.html?id=${item._id}`;
+
+    const articleElement = document.createElement('article');
+    productElement.appendChild(articleElement);
+
+    const imageElement = document.createElement('img');
+    imageElement.setAttribute('src', item.imageUrl);
+    imageElement.setAttribute('alt', item.altTxt);
+
+    articleElement.appendChild(imageElement);
+
+    articleElement.innerHTML += `
         <h3 class="productName">${item.name}</h3>
         <p class="productDescription">${item.description}</p>
       `;
-      itemsHolder.appendChild(productElement);
-    }
+    itemsHolder.appendChild(productElement);
   }
+}
